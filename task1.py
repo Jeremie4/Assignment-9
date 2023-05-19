2bf0539ad2feacee8bfd03741f5b09a4f52b3ff3
Jeremie4 <126706125+Jeremie4@users.noreply.github.com>
2023-05-19 10:48:27 -0600

    Updated code

--- task1.py
+++ task1.py
@@ -7,28 +7,33 @@ class Face:
         self.__happy = True
         self.__darkEyes = True
 
-    def __drawHead(self):
-        t.setpos(0,0)
-        t.circle(100)
-
-    def __drawEyes(self):
-        t.setpos(-50,0)
-        t.circle(5)
-        t.setpos(50,0)
-        t.circle(5)
-
-    def __drawMouth(self):
-        t.setpos(-50,-75)
-        t.circle(50, 180)
-
     def draw_face(self):
         t.clear()
         self.__drawHead()
         self.__drawEyes()
         self.__drawMouth()
+    
+    def __drawHead(self):
+        t.pu
+        t.setpos(0,0)
+        t.pd
+        t.circle(100)
+
+    def __drawEyes(self):
+        t.pu
+        t.setpos(25,100)
+        t.pd
+        t.circle(5)
+        t.pu
+        t.setpos(-25,100)
+        t.pd
+        t.circle(5)
+
+    def __drawMouth(self):
+        pass
 
     def isSmile(self):
-        drawsmile = True if self.__smile else False
+        pass
 
     def isHappy(self):
         pass
@@ -50,16 +55,16 @@ class Face:
 
 
 def main():
-    face = True
-    face.denominator
+    face = Face()
+    face.draw_face()
 
     done = False
 
     while not done:
         print("Change My Face")
-        mouth = "frown" + ", " + "smile"
-        emotion = "angry" + ", " + "happy"
-        eyes = "blue" + ", " + "black"
+        mouth = "frown" or "smile"
+        emotion = "angry" or "happy"
+        eyes = "blue" or "black"
         print("1) Make me", mouth)
         print("2) Make me", emotion)
         print("3) Make my eyes", eyes)
@@ -78,7 +83,7 @@ def main():
 
     print("Thanks for Playing")
 
-    t.hideturtle()
+    t.hidet()
     t.done()
 
 
